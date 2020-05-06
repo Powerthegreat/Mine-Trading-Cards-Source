c055055a31add141be7317425b095853420c708d
Power <powerfinnthegreat@gmail.com>
2020-05-06 10:53:38 +1000

    Adjusted prices for card packs bought from villagers

--- java/com/is/mtc/village/VillageHandler.java
+++ java/com/is/mtc/village/VillageHandler.java
@@ -23,12 +23,12 @@ public class VillageHandler
 	public void manipulateTradesForVillager(EntityVillager villager, MerchantRecipeList recipeList, Random random) {
 		if (villager.getProfession() == TRADER_ID) {
 			recipeList.clear();
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.iron_ingot, 1), new ItemStack(MineTradingCards.packCommon)));
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.gold_ingot, 1), new ItemStack(MineTradingCards.packUncommon)));
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.diamond, 1), new ItemStack(MineTradingCards.packRare)));
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.iron_ingot, 2), new ItemStack(MineTradingCards.packStandard)));
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.iron_ingot, 2), new ItemStack(MineTradingCards.packEdition)));
-			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.gold_ingot, 2), new ItemStack(Items.emerald, 1), new ItemStack(MineTradingCards.packAncient)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.emerald, 1), new ItemStack(MineTradingCards.packCommon, random.nextInt(3) + 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.emerald, random.nextInt(2) + 1), new ItemStack(MineTradingCards.packUncommon)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.emerald, random.nextInt(4) + 3), new ItemStack(MineTradingCards.packRare)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.emerald, 1), new ItemStack(MineTradingCards.packStandard, random.nextInt(2) + 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.emerald, 1), new ItemStack(MineTradingCards.packEdition, random.nextInt(2) + 1)));
+			recipeList.addToListWithCheck(new MerchantRecipe(new ItemStack(Items.emerald, random.nextInt(6) + 5), new ItemStack(MineTradingCards.packAncient)));
 		}
 	}
 }
